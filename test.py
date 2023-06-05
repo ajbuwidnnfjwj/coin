8fc7acd3582859fd8decc5edea68272132763f18
ajbuwidnnfjwj <416tommy@naver.com>
2023-06-05 23:58:10 +0900

    deleting unnecessary files

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "your-access"          # 본인 값으로 변경
-secret = "your-secret"          # 본인 값으로 변경
+access = "amsKD2aTZurXzcG8eVYCcHgBdx8aijgrzB6XGL7B"          # 본인 값으로 변경
+secret = "FeuIjVwUs0VkhAWlFkZMN3oqh3mMBFnPPDsPCYjl"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
