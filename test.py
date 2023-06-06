892d525f6c685238f69ccd2d95e30501429c032d
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2023-06-07 03:25:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "amsKD2aTZurXzcG8eVYCcHgBdx8aijgrzB6XGL7B"          # 본인 값으로 변경
-secret = "FeuIjVwUs0VkhAWlFkZMN3oqh3mMBFnPPDsPCYjl"          # 본인 값으로 변경
+access = "access"          # 본인 값으로 변경
+secret = "secret"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 df = pyupbit.get_ohlcv("KRW-BTC", interval="day", count=2)
